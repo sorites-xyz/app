8a46842cea956291192fcb649de680bd02825c22
Ghaith Mansour <ghaith.b.mansour.dev@hotmail.com>
2025-03-14 23:09:02 +0400

    change home page title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Sorites</title>
+        <title>InReal</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
